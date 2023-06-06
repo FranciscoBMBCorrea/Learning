f27d79f1708bf8a87a265811ad9a60471fd5048d
franciscoCorrea <franciscobmbc@gmail.com>
2023-06-06 18:33:19 +0100

    add enemy class

--- Free Code Camp/Monster Game/script.js
+++ Free Code Camp/Monster Game/script.js
@@ -206,6 +206,16 @@ window.addEventListener('load', function () {
         }
     }
 
+    class Enemy {
+        constructor(game){
+            this.game = game;
+            this.collisionRadius = 30;
+            this.collisionX = this.game.width;
+            this.collisionY = Math.random() * this.game.height;
+            this.speedX = Math.random() * 3 + 0.5;
+        }
+    }
+
     class Game {
         constructor(canvas) {
             this.canvas = canvas;
@@ -275,7 +285,6 @@ window.addEventListener('load', function () {
                     object.draw(context);
                     object.update();
                 });
-                // sort by vertical position
 
                 this.timer = 0;
 
