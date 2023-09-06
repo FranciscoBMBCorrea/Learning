c9234e0c9a1d34b1602e5de8b0a0f2aed55f7b1f
franciscoCorrea <franciscobmbc@gmail.com>
2023-09-06 18:13:28 +0100

    add words and import it to the App file

--- Youtube/react-TS-hangman/src/App.tsx
+++ Youtube/react-TS-hangman/src/App.tsx
@@ -1,5 +1,8 @@
+import { useState } from "react"
+import words from "./wordList.json"
+
 function App() {
-  const [wordToGuess, setWordToGuess] = useState("test");
+  const [wordToGuess, setWordToGuess] = useState(() => {return words[Math.floor(Math.random() + words.length)]});
 
   return (
     <h1>Hi</h1>
