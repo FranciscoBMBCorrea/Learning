cea5b22355c8be1901c0ca70746fac5496dfcd6e
franciscoCorrea <franciscobmbc@gmail.com>
2023-09-06 18:34:14 +0100

    add a styles.css file

--- Youtube/react-TS-hangman/src/App.tsx
+++ Youtube/react-TS-hangman/src/App.tsx
@@ -1,11 +1,17 @@
 import { useState } from "react"
 import words from "./wordList.json"
+import "./styles.css"
 
 function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {return words[Math.floor(Math.random() + words.length)]});
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
 
   return (
-    <h1>Hi</h1>
+<div className="game">
+  <div className="lose-win">Lose Win</div>
+</div>
+
   )
 }
 
