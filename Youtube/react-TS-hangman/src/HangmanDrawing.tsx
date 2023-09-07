edbbd498aabe1c77349c0ba688bfbb413d201880
franciscoCorrea <franciscobmbc@gmail.com>
2023-09-07 19:59:15 +0100

    add inactive and active letters and Winner and Loser text

--- Youtube/react-TS-hangman/src/HangmanDrawing.tsx
+++ Youtube/react-TS-hangman/src/HangmanDrawing.tsx
@@ -9,15 +9,16 @@ const LEFT_ARM = ( <div className="left-arm"></div> );
 const RIGHT_LEG = ( <div className="right-leg"></div>);
 const LEFT_LEG = ( <div className="left-leg"></div>);
 
-export function HangmanDrawing() {
+const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG];
+
+type HangmanDrawingProps = {
+    numberOfGuesses: number;
+};
+
+export function HangmanDrawing({ numberOfGuesses } : HangmanDrawingProps) {
     return (
         <div className="set">
-            {HEAD}
-            {BODY}
-            {RIGHT_ARM}
-            {LEFT_ARM}
-            {RIGHT_LEG}
-            {LEFT_LEG}
+            {BODY_PARTS.slice(0, numberOfGuesses)}
             <div className="rope"></div>
             <div className="post"></div>
             <div className="mast"></div>
--- Youtube/react-TS-hangman/src/HangmanWord.tsx
+++ Youtube/react-TS-hangman/src/HangmanWord.tsx
@@ -1,25 +1,29 @@
 import "./css/word.css"
 
+type HangmanWordProps = {
+    guessedLetters: string[]
+    wordToGuess: string
+    reveal?: boolean 
+}
 
-export function HangmanWord() {
-
-    const word = "test"
-    const guessedLetters = ["t", "e", "g"];
+export function HangmanWord({ guessedLetters, wordToGuess, reveal= false }: HangmanWordProps) {
 
     return (
         <div className="word">
-            {word.split("").map((letter, index) => (
-            <span className="letters" key={index}>
-                <span 
-                style={{
-                    visibility: guessedLetters.includes(letter)
-                    ? "visible"
-                    : "hidden",
-                }}
-                >
+            {wordToGuess.split("").map((letter, index) => (
+                <span className="letters" key={index}>
+                    <span
+                        style={{
+                            visibility: guessedLetters.includes(letter) || reveal
+                                ? "visible"
+                                : "hidden",
+                                color:
+                                !guessedLetters.includes(letter) && reveal ? "red" : "black",
+                        }}
+                    >
 
-                {letter}
-                </span>
+                        {letter}
+                    </span>
                 </span>
             ))}
         </div>
