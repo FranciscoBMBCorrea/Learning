a8422eb95abd8cf59ab7dcbef5bcbea328ff54f1
franciscoCorrea <franciscobmbc@gmail.com>
2023-09-07 11:21:31 +0100

    add word.css file and finished the hangmanword

--- Youtube/react-TS-hangman/src/HangmanWord.tsx
+++ Youtube/react-TS-hangman/src/HangmanWord.tsx
@@ -1,4 +1,27 @@
+import "./css/word.css"
+
 
 export function HangmanWord() {
 
+    const word = "test"
+    const guessedLetters = ["t", "e", "g"];
+
+    return (
+        <div className="word">
+            {word.split("").map((letter, index) => (
+            <span className="letters" key={index}>
+                <span 
+                style={{
+                    visibility: guessedLetters.includes(letter)
+                    ? "visible"
+                    : "hidden",
+                }}
+                >
+
+                {letter}
+                </span>
+                </span>
+            ))}
+        </div>
+    )
 }
