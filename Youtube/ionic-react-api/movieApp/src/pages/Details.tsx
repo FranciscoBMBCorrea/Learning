99a4cb099e57072217fbe189445f0d035b78ce83
FranciscoBMBCorrea <franciscobmbc@gmail.com>
2023-10-27 20:12:52 +0100

    add final documentation

--- Youtube/ionic-react-api/movieApp/src/pages/Details.tsx
+++ Youtube/ionic-react-api/movieApp/src/pages/Details.tsx
@@ -30,15 +30,23 @@ import {
   trophyOutline,
 } from "ionicons/icons";
 
+// Defining the type of props received by the Details component
 interface DetailsPageProps
   extends RouteComponentProps<{
     id: string;
   }> {}
 
+// The Details component
 const Details: React.FC<DetailsPageProps> = ({ match }) => {
+  // Using the custom hook to fetch details data from the API
   const { getDetails } = useApi();
+  
+  // State to hold the information about the movie details
   const [information, setInformation] = useState<DetailsResult | null>(null);
+
+  // Use Ionic lifecycle hook to fetch data when the view is about to enter
   useIonViewWillEnter(() => {
+    // Function to load data from the API
     const loadData = async () => {
       const id = match.params.id;
       const data = await getDetails(id);
@@ -46,29 +54,37 @@ const Details: React.FC<DetailsPageProps> = ({ match }) => {
       console.log("🚀 ~file: Details.tsx:26 ~useIonViewWillEnter ~data", data);
     };
 
+    // Call the function to load data
     loadData();
   });
 
+  // JSX structure of the component
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
+          {/* Back button to navigate back to the movies page */}
           <IonButtons slot="start">
             <IonBackButton defaultHref="/movies"></IonBackButton>
           </IonButtons>
+          {/* Displaying the genre as the title in the header */}
           <IonTitle>{information?.Genre}</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
+        {/* Displaying movie details if available */}
         {information && (
           <IonCard>
             <IonCardHeader>
+              {/* Displaying movie title and year */}
               <IonCardTitle>{information.Title}</IonCardTitle>
               <IonCardSubtitle>{information.Year}</IonCardSubtitle>
             </IonCardHeader>
             <IonCardContent>
+              {/* Displaying movie poster */}
               <IonImg src={information.Poster} />
 
+              {/* Displaying IMDb rating */}
               <IonItem lines="none">
                 <IonIcon icon={starHalfOutline} slot="start" color="warning" />
                 <IonLabel>{information.imdbRating}</IonLabel>
@@ -77,22 +93,26 @@ const Details: React.FC<DetailsPageProps> = ({ match }) => {
           </IonCard>
         )}
 
+        {/* Modal to show additional details about the movie */}
         <IonModal
           trigger="open-modal"
           initialBreakpoint={0.25}
           breakpoints={[0, 0.25, 0.5, 0.75]}
         >
           <IonContent className="ion-padding">
+            {/* Displaying director information */}
             <IonItem lines="none">
               <IonIcon icon={clipboardOutline} slot="start" />
               <IonLabel>{information?.Director}</IonLabel>
             </IonItem>
+            {/* Displaying actors information */}
             <IonItem lines="none">
               <IonIcon icon={bodyOutline} slot="start" />
               <IonLabel className="ion-text-wrap">
                 {information?.Actors}
               </IonLabel>
             </IonItem>
+            {/* Displaying awards and plot information */}
             <IonItem lines="none">
               <IonIcon icon={trophyOutline} slot="start" />
               <IonLabel>{information?.Awards}</IonLabel>
@@ -101,6 +121,7 @@ const Details: React.FC<DetailsPageProps> = ({ match }) => {
           </IonContent>
         </IonModal>
       </IonContent>
+      {/* Footer button to open the modal */}
       <IonFooter>
         <IonButton expand="full" id="open-modal">
           Show More
@@ -110,4 +131,5 @@ const Details: React.FC<DetailsPageProps> = ({ match }) => {
   );
 };
 
+// Exporting the Details component
 export default Details;
